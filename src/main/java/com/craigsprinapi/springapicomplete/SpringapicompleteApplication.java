e183c2cd9fca2151191ec377d0c3a2a107fdae13
leave20 <thetai6102@gmail.com>
2020-11-10 16:56:58 -0500

    Initial commit

---
+++ src/main/java/com/craigsprinapi/springapicomplete/SpringapicompleteApplication.java
@@ -0,0 +1,13 @@
+package com.craigsprinapi.springapicomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapicompleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringapicompleteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/craigsprinapi/springapicomplete/SpringapicompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craigsprinapi.springapicomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapicompleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
